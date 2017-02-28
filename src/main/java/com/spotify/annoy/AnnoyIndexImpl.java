8f78be912bcb1f2e216425835fb829e85a613d3b
Romain Yon <romain@spotify.com>
2017-02-28 11:47:52 -0500

    dummy implementaiton

---
+++ src/main/java/com/spotify/annoy/AnnoyIndexImpl.java
@@ -0,0 +1,48 @@
+package com.spotify.annoy;
+
+public class AnnoyIndexImpl implements AnnoyIndex {
+
+  public AnnoyIndex addItem(int i, float[] vector) {
+    return null;
+  }
+
+  public int[] getNearestByVector(float[] vector, int n) {
+    return new int[0];
+  }
+
+  public int[] getNearestByVector(float[] vector, int n, int searchK) {
+    return new int[0];
+  }
+
+  public int[] getNearestByItem(float[] vector, int n) {
+    return new int[0];
+  }
+
+  public int[] getNearestByItem(float[] vector, int n, int searchK) {
+    return new int[0];
+  }
+
+  public AnnoyIndex build(int nTrees) {
+    return null;
+  }
+
+  public AnnoyIndex save(String filename) {
+    return null;
+  }
+
+  public AnnoyIndex load(String filename) {
+    return null;
+  }
+
+  public float[] getItemVector(int i) {
+    return new float[0];
+  }
+
+  public float getDistance(int i, int j) {
+    return 0;
+  }
+
+  public int size() {
+    return 0;
+  }
+}
