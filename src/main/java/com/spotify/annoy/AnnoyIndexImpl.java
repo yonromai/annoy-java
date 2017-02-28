2a9fc10044e046083cf1aac7e8e2eaeff46cb288
Romain Yon <romain@spotify.com>
2017-02-28 14:05:25 -0500

    moar methods

--- src/main/java/com/spotify/annoy/AnnoyIndexImpl.java
+++ src/main/java/com/spotify/annoy/AnnoyIndexImpl.java
@@ -6,55 +6,76 @@ public class AnnoyIndexImpl implements AnnoyIndex {
     System.loadLibrary("annoy"); // Load native library at runtime
   }
 
-  private native void cppAddItem(int i, float[] vector);
-
-
   public AnnoyIndexImpl() {
   }
 
+  private native void cppAddItem(int i, float[] vector);
 
   public AnnoyIndex addItem(int i, float[] vector) {
     cppAddItem(i, vector);
-    return null;
+    return this;
   }
+
+  private native int[] cppGetNearestByVector(float[] vector, int n);
 
   public int[] getNearestByVector(float[] vector, int n) {
-    return new int[0];
+    return cppGetNearestByVector(vector, n);
   }
+
+  private native int[] cppGetNearestByVector(float[] vector, int n, int searchK);
 
   public int[] getNearestByVector(float[] vector, int n, int searchK) {
-    return new int[0];
+    return cppGetNearestByVector(vector, n, searchK);
   }
+
+  private native int[] cppGetNearestByItem(float[] vector, int n);
 
   public int[] getNearestByItem(float[] vector, int n) {
-    return new int[0];
+    return cppGetNearestByItem(vector, n);
   }
+
+  private native int[] cppGetNearestByItem(float[] vector, int n, int searchK);
 
   public int[] getNearestByItem(float[] vector, int n, int searchK) {
-    return new int[0];
+    return cppGetNearestByItem(vector, n, searchK);
   }
+
+  private native void cppBuild(int nTrees);
 
   public AnnoyIndex build(int nTrees) {
-    return null;
+    cppBuild(nTrees);
+    return this;
   }
+
+  private native void cppSave(String filename);
 
   public AnnoyIndex save(String filename) {
-    return null;
+    cppSave(filename);
+    return this;
   }
+
+  private native void cppLoad(String filename);
 
   public AnnoyIndex load(String filename) {
-    return null;
+    cppLoad(filename);
+    return this;
   }
+
+  private native float[] cppGetItemVector(int i);
 
   public float[] getItemVector(int i) {
-    return new float[0];
+    return cppGetItemVector(i);
   }
+
+  private native float cppGetDistance(int i, int j);
 
   public float getDistance(int i, int j) {
-    return 0;
+    return cppGetDistance(i, j);
   }
 
+  private native int cppSize();
+
   public int size() {
-    return 0;
+    return cppSize();
   }
 }
