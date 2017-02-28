cee0610dec3c95f6ea302366d76819363e11eb31
Shaun Cheng <scheng@spotify.com>
2017-02-28 15:19:56 -0500

    constructor and build implemented

--- src/main/resources/jni/com_spotify_annoy_AnnoyIndexImpl.cpp
+++ src/main/resources/jni/com_spotify_annoy_AnnoyIndexImpl.cpp
@@ -4,8 +4,19 @@
 #include <annoylib.h>
 
 
+namespace
+{
+    static AnnoyIndexInterface<int, float>* annoy_index;
+}
+
+JNIEXPORT void JNICALL Java_com_spotify_annoy_AnnoyIndexImpl_cppCtor
+  (JNIEnv *env, jobject obj, jint jni_int)
+{
+    annoy_index = new AnnoyIndex<int, float, Angular, RandRandom>(jni_int);
+}
 
 
+//TODO
 JNIEXPORT void JNICALL Java_com_spotify_annoy_AnnoyIndexImpl_cppAddItem
   (JNIEnv *env, jobject jniObj, jint inJNIInt, jfloatArray inJNIFloatArray)
 {
@@ -14,27 +25,20 @@ JNIEXPORT void JNICALL Java_com_spotify_annoy_AnnoyIndexImpl_cppAddItem
    if (NULL == inCArray) return;
    jsize length = env->GetArrayLength(inJNIFloatArray);
 
-   jint inCint = env->GetIntField(inJNIInt, NULL);
-
-
    for(int i=0; i<length; ++i) {
        float d = inCArray[i];
        printf("in C++, value of the array element####:%f\n", d);
    }
-
-   
-
-   AnnoyIndexInterface<int, float>* ai = new AnnoyIndex<int, float, Angular, RandRandom>(10);
-
-   ai->build(12342);
-
-
-
-
-
-
-
-
-   env->ReleaseFloatArrayElements(inJNIArray, inCArray, 0); // release resources
+   env->ReleaseFloatArrayElements(inJNIFloatArray, inCArray, 0); // release resources
    return;
 }
+
+
+
+JNIEXPORT void JNICALL Java_com_spotify_annoy_AnnoyIndexImpl_cppBuild
+(JNIEnv *env, jobject jni_obj, jint jni_int)
+{
+    return annoy_index->build(jni_int);
+}
+
+
