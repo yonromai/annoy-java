af49e63b77d6417c1515013cf11f313a1ef19160
Romain Yon <romain@spotify.com>
2017-03-02 23:31:09 -0500

    clean up AnnoyImpl

--- src/main/java/com/spotify/annoy/AnnoyIndexImpl.java
+++ src/main/java/com/spotify/annoy/AnnoyIndexImpl.java
@@ -1,30 +1,58 @@
 package com.spotify.annoy;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.Arrays;
 import java.util.List;
-import org.apache.commons.lang3.ArrayUtils;
 
 class AnnoyIndexImpl implements AnnoyIndex {
 
-  private final int f;
+  private final int dim;
 
-  private native void cppCtor(int f);
-
-  AnnoyIndexImpl(int f) {
-    this.f = f;
-    final String dir = System.getProperty("java.library.path");
-    //set explicit path for our custom library
-    System.load(dir + "/libannoy.jnilib"); //TODO: linux name is different.
-    cppCtor(f);
+  public List<Integer> getNearestByVector(List<Float> vector, int n) {
+    validateVecSize(vector);
+    return primitiveToBoxed(cppGetNearestByVector(boxedToPrimitive(vector), n));
   }
 
-  private native void cppSetSeed(int seed);
-  AnnoyIndex setSeed(int seed) {
-    cppSetSeed(seed);
+  public List<Integer> getNearestByVectorK(List<Float> vector, int n, int searchK) {
+    validateVecSize(vector);
+    return primitiveToBoxed(cppGetNearestByVectorK(boxedToPrimitive(vector), n, searchK));
+  }
+
+  public List<Integer> getNearestByItem(int item, int n) {
+    return primitiveToBoxed(cppGetNearestByItem(item, n));
+  }
+
+  public List<Integer> getNearestByItemK(int item, int n, int searchK) {
+    return primitiveToBoxed(cppGetNearestByItemK(item, n, searchK));
+  }
+
+  public AnnoyIndex save(String filename) {
+    cppSave(filename);
     return this;
   }
 
-  private native void cppAddItem(int i, float[] vector);
+  public List<Float> getItemVector(int i) {
+    return primitiveToBoxed(cppGetItemVector(i));
+  }
+
+  public float getDistance(int i, int j) {
+    return cppGetDistance(i, j);
+  }
+
+  public int size() {
+    return cppSize();
+  }
+
+  // Construction
+
+  AnnoyIndexImpl(int dim) {
+    this.dim = dim;
+    final String dir = System.getProperty("java.library.path");
+    //set explicit path for our custom library
+    System.load(dir + "/libannoy.jnilib"); //TODO: linux name is different.
+    cppCtor(dim);
+  }
 
   AnnoyIndex addItem(int i, List<Float> vector) {
     validateVecSize(vector);
@@ -40,72 +68,22 @@ class AnnoyIndexImpl implements AnnoyIndex {
     return this;
   }
 
-  private native int[] cppGetNearestByVector(float[] vector, int n);
-
-  public List<Integer> getNearestByVector(List<Float> vector, int n) {
-    validateVecSize(vector);
-    return primitiveToBoxed(cppGetNearestByVector(boxedToPrimitive(vector), n));
-  }
-
-  private native int[] cppGetNearestByVectorK(float[] vector, int n, int searchK);
-
-  public List<Integer> getNearestByVectorK(List<Float> vector, int n, int searchK) {
-    validateVecSize(vector);
-    return primitiveToBoxed(cppGetNearestByVectorK(boxedToPrimitive(vector), n, searchK));
-  }
-
-  private native int[] cppGetNearestByItem(int item, int n);
-
-  public List<Integer> getNearestByItem(int item, int n) {
-    return primitiveToBoxed(cppGetNearestByItem(item, n));
-  }
-
-  private native int[] cppGetNearestByItemK(int item, int n, int searchK);
-
-  public List<Integer> getNearestByItemK(int item, int n, int searchK) {
-    return primitiveToBoxed(cppGetNearestByItemK(item, n, searchK));
-  }
-
-  private native void cppBuild(int nTrees);
-
   AnnoyIndex build(int nTrees) {
     cppBuild(nTrees);
     return this;
   }
-
-  private native void cppSave(String filename);
-
-  public AnnoyIndex save(String filename) {
-    cppSave(filename);
-    return this;
-  }
-
-  private native void cppLoad(String filename);
 
   AnnoyIndex load(String filename) {
     cppLoad(filename);
     return this;
   }
 
-  private native float[] cppGetItemVector(int i);
-
-  public List<Float> getItemVector(int i) {
-    return primitiveToBoxed(cppGetItemVector(i));
+  AnnoyIndex setSeed(int seed) {
+    cppSetSeed(seed);
+    return this;
   }
 
-  private native float cppGetDistance(int i, int j);
-
-  public float getDistance(int i, int j) {
-    return cppGetDistance(i, j);
-  }
-
-  private native int cppSize();
-
-  public int size() {
-    return cppSize();
-  }
-
-  // helpers
+  // Helpers
 
   private static List<Float> primitiveToBoxed(float[] v) {
     return Arrays.asList(ArrayUtils.toObject(v));
@@ -120,8 +98,36 @@ class AnnoyIndexImpl implements AnnoyIndex {
   }
 
   private void validateVecSize(List<Float> vector) {
-    if (vector.size() != f) {
+    if (vector.size() != dim) {
       throw new RuntimeException("Item's vector should match the dimension of the tree");
     }
   }
+
+  // Native cpp  methods
+
+  private native void cppCtor(int f);
+
+  private native void cppAddItem(int i, float[] vector);
+
+  private native int[] cppGetNearestByVector(float[] vector, int n);
+
+  private native int[] cppGetNearestByVectorK(float[] vector, int n, int searchK);
+
+  private native int[] cppGetNearestByItem(int item, int n);
+
+  private native int[] cppGetNearestByItemK(int item, int n, int searchK);
+
+  private native void cppBuild(int nTrees);
+
+  private native void cppSave(String filename);
+
+  private native void cppLoad(String filename);
+
+  private native float[] cppGetItemVector(int i);
+
+  private native float cppGetDistance(int i, int j);
+
+  private native int cppSize();
+
+  private native void cppSetSeed(int seed);
 }
